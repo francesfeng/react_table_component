ee52aa0f159ca944c045f44cde8b5f9ed9fab715
francesfeng <ffan1201@hotmail.com>
2022-02-10 18:18:27 +0000

    correct build path

--- react_table/__init__.py
+++ react_table/__init__.py
@@ -6,12 +6,19 @@ import streamlit.components.v1 as components
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
 
+#st.write(build_dir)
+
 _selectable_data_table = components.declare_component(
-    "react_table", url=build_dir,
+
+    #"selectable_data_table", url="http://localhost:3001",
+
+    "selectable_data_table", path=build_dir,
 )
 
 
+
 def selectable_data_table(data, key=None):
+    st.write(build_dir)
     return _selectable_data_table(data=data, default=[], key=key)
 
 
